2dcb777abffdcfc18d8fae5b4bf2ee6ef3c52987
Shawn Ferry <sferry@noroute.local>
2016-03-14 12:52:57 -0400

    spec fixups, general, dns
    Address and zone types, providers and tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,6 @@
-#dir = File.expand_path(File.dirname(__FILE__))
-#$LOAD_PATH.unshift File.join(dir, 'lib')
 require 'rubygems'
 require 'puppetlabs_spec_helper/module_spec_helper'
 
 RSpec.configure do |config|
-#    config.mock_with :mocha
     config.example_status_persistence_file_path = 'spec/examples.txt'
 end
