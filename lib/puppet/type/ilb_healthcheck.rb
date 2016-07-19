98052ea1e85fe6a6e375185edfce36172a5d124c
Shawn Ferry <sferry@gmail.com>
2016-07-19 13:18:07 -0400

    ilb_healthcheck type spec

--- lib/puppet/type/ilb_healthcheck.rb
+++ lib/puppet/type/ilb_healthcheck.rb
@@ -30,31 +30,31 @@ Puppet::Type.newtype(:ilb_healthcheck) do
     desc "Threshold at which a test is considered failed
       following interim failures of hc-test. If you kill an hc-test test, the
       result is considered a failure. The default value is five seconds."
-      newvalue(/\d\+/)
+      newvalue(/^\d+$/)
   end
 
   newproperty(:count) do
     desc "Maximum number of attempts to run hc-test before
       marking a server as down. The default value is three iterations."
-      newvalue(/\d\+/)
+      newvalue(/^\d+$/)
   end
 
   newproperty(:interval) do
     desc "Interval between invocations of hc-test. This value must be greater
       than hc-timeout times hc-count. The default value is 30 seconds."
-      newvalue(/\d\+/)
+      newvalue(/^\d+$/)
   end
 
   newproperty(:test) do
     desc "UDP, TCP, external method (script or binary). An external method
       should be specified with a full path name."
-      newvalue(:tcp,:udp,%r(^/.+))
+      newvalues(:tcp,:udp,%r(^/.+))
   end
 
   newproperty(:default_ping) do
     desc "Execute default ping test before high layer health check tests.
     Default: true"
     defaultto :true
-    newvalue(:true,:false)
+    newvalues(:true,:false)
   end
 end
