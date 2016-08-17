d99831216a036bd9626cfc8a3a86ae1a432fa981
Shawn Ferry <sferry@gmail.com>
2016-08-17 14:07:30 -0400

    ilb_healthcheck working

---
+++ lib/puppet/provider/ilb_healthcheck/solaris.rb
@@ -0,0 +1,91 @@
+
+#
+# Copyright (c) 2016, Oracle and/or its affiliates. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+Puppet::Type.type(:ilb_healthcheck).provide(:ilb_healthcheck) do
+  @doc = "Provider to manage Solaris Integrated Load Balancer (ILB) health checks."
+  confine :operatingsystem => [:solaris]
+  defaultfor :osfamily => :solaris, :kernelrelease => ['5.11', '5.12']
+  commands :ilbadm => '/usr/sbin/ilbadm'
+
+  mk_resource_methods
+
+  def self.instances
+    checks=[]
+
+    ilbadm('show-healthcheck').each_line { |line|
+     next if line.match(/^HCNAME/)
+     name, timeout, count, interval, default_ping, test = line.strip.split(/\s+/,6)
+        checks.push(
+          new(:name => name,
+              :timeout => timeout,
+              :count => count,
+              :interval => interval,
+              :default_ping => (default_ping == "Y" ? :true : :false),
+              :test => test,
+              :ensure => :present
+             )
+        )
+    }
+    return checks
+  end
+
+  def self.prefetch(resources)
+    _instances = instances
+    resources.keys.each do |name|
+      if provider = _instances.find { |_resource| _resource.name == name }
+        resources[name].provider = provider
+      end
+    end
+  end
+
+  def exists?
+    @property_hash[:ensure] == :present
+  end
+
+  def create
+    _args = []
+
+    if @resource[:default_ping] == :false
+      _args.push "-n"
+    end
+
+    _arg_str = [:timeout, :count, :interval, :test].each.collect { |thing|
+      if @resource[thing] && @resource[thing] != :absent
+        "hc-#{thing}=#{@resource[thing]}"
+      end
+    }
+
+    _args.push('-h',_arg_str.compact.join(','))
+
+    ilbadm('create-healthcheck', *_args, @resource[:name] )
+    nil
+  end
+
+  def destroy
+    ilbadm("delete-healthcheck", @resource[:name])
+  end
+
+  [
+    :timeout, :count, :interval, :test, :default_ping
+  ].each { |method|
+    define_method("#{method}=") do |should|
+      destroy
+      create
+      @property_hash[method] = should
+    end
+  }
+end
--- lib/puppet/type/ilb_healthcheck.rb
+++ lib/puppet/type/ilb_healthcheck.rb
@@ -18,7 +18,14 @@
 require File.expand_path(File.join(File.dirname(__FILE__), '..','..','puppet_x/oracle/solaris_providers/util/validation.rb'))
 
 Puppet::Type.newtype(:ilb_healthcheck) do
-  @doc = "Manage Solaris Integrated Load Balancer (ILB) health check configuration"
+  @doc = "Manage Solaris Integrated Load Balancer (ILB) health check configuration.
+  See: ilbadm(8)
+
+  **Note**
+  * Healthchecks cannot be removed/modified while they are assigned to rules
+  * Healthchecks cannot be modified only destroyed and recreated
+  * Attempts to modify assigned healthchecks will result in catalog run errors
+  "
 
   ensurable
 
@@ -47,7 +54,24 @@ Puppet::Type.newtype(:ilb_healthcheck) do
 
   newproperty(:test) do
     desc "UDP, TCP, external method (script or binary). An external method
-      should be specified with a full path name."
+      should be specified with a full path name.
+
+      Arguments to external methods
+       $1 VIP (literal IPv4 or IPv6 address)
+       $2 Server IP (literal IPv4 or IPv6 address)
+       $3 Protocol (UDP, TCP as a string)
+       $4 Numeric port range (the user-specified value for hc-port)
+       $5 Maximum time (in seconds) that the test must wait before returning
+          a failure. If the test runs beyond the specified time, it might
+          be stopped, and the test is considered failed. This value is
+          user-defined and specified in hc-timeout.
+
+      The user-supplied test, my-script, may or may not use all the arguments,
+      but it must return one of the following:
+       Round-trip time (RTT) in microseconds
+       0 if the test does not calculate RTT
+       -1 for failure
+    "
       newvalues(:tcp,:udp,%r(^/.+))
   end
 
