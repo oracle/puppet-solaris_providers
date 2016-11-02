be636920ad26491fb84e07fd705e41a59e54c54a
Shawn Ferry <sferry@gmail.com>
2016-11-02 19:58:29 -0400

    24696809 Puppet link aggregation modules cascading errors

--- lib/puppet/type/address_object.rb
+++ lib/puppet/type/address_object.rb
@@ -31,7 +31,7 @@ Puppet::Type.newtype(:address_object) do
     newparam(:temporary) do
         desc "Optional parameter that specifies that the address object is
               temporary.  Temporary address objects last until the next reboot."
-        default(:false)
+        #defaultto :false
         newvalues(:true, :false)
     end
 
